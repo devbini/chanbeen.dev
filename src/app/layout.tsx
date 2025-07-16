7bf684bc047b1875b87f267144003ebc65522d04
devbini <cong8685@naver.com>
2025-07-16 18:41:35 +0900

    feat: 1차openGraph 적용
    기본 커버 이미지 및 설명 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,21 @@ export const metadata: Metadata = {
         "google-site-verification": "uD9P9Pv7oLPV7ZpTN031Hi6BkIPvlGb0w1rW9FMQXFY",
         "naver-site-verification": "1ead47515e0286ec0bb8f1c02807f56135d84c9a",
     },
+    openGraph: {
+        title: "💻 김찬빈 기술 블로그",
+        description: "기술과 경험을 함께 공유합니다.",
+        url: "https://chanbeen.com",
+        siteName: "김찬빈.dev 💻",
+        images: [{
+            url: "https://chanbeen.com/blog_shared_cover.png",
+            width: 1200,
+            height: 630,
+            alt: "블로그 커버 이미지",
+        },
+        ],
+        type: "website",
+        locale: "ko_KR"
+    },
 };
 
 export default function RootLayout({
