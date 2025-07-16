f138f0cf9283022ec5b87d86ea762aa52c22bda3
devbini <cong8685@naver.com>
2025-07-16 18:21:17 +0900

    feat: SEO-서치콘솔 키 메타 등록
    Google, Naver만 우선 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,10 @@ const inter = Inter({subsets: ["latin"]});
 export const metadata: Metadata = {
     title: "김찬빈.dev 💻",
     description: "Dev-bini's Tech Blog.",
+    other: {
+        "google-site-verification": "uD9P9Pv7oLPV7ZpTN031Hi6BkIPvlGb0w1rW9FMQXFY",
+        "naver-site-verification": "1ead47515e0286ec0bb8f1c02807f56135d84c9a",
+    },
 };
 
 export default function RootLayout({
