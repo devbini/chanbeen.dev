ef89cb448697a7016865dc3f74fe3153840032c7
devbini <cong8685@naver.com>
2026-01-15 12:04:24 +0900

    style: 메타데이터 및 커버 이미지 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,23 +8,23 @@ import StyledComponentsRegistry from '@/lib/registry';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: '김찬빈.dev 💻',
-    description: "Dev-bini's Tech Blog.",
+    title: '찬빈.com',
+    description: "Chanbeen's Tech Blog.",
     other: {
         'google-site-verification': 'uD9P9Pv7oLPV7ZpTN031Hi6BkIPvlGb0w1rW9FMQXFY',
         'naver-site-verification': '1ead47515e0286ec0bb8f1c02807f56135d84c9a',
     },
     openGraph: {
-        title: '💻 김찬빈 기술 블로그',
+        title: '김찬빈 기술 블로그',
         description: '기술과 경험을 함께 공유합니다.',
         url: 'https://chanbeen.com',
-        siteName: '김찬빈.dev 💻',
+        siteName: '찬빈.com',
         images: [
             {
                 url: 'https://chanbeen.com/blog_shared_cover.png',
                 width: 1200,
                 height: 630,
-                alt: '블로그 커버 이미지',
+                alt: '커버',
             },
         ],
         type: 'website',
